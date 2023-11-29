af3343e6bb1dbed0fd289580e48d63eef07a7aef
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-11-29 13:47:58 +0330

    third part

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,31 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+type Product = {
+  id:string,
+  title: string
+}
+
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+  let products = ref([] as Product[])
+  
+  function addProduct (id:string, title:string) {
+    products.value.push({id, title})
   }
 
-  return { count, doubleCount, increment }
+  function removeProduct (id:string) {
+    for (let i = 0; i < products.value.length; i++) {
+      if (products[i].value.id === id) {
+        for(let j = i + 1; j < products.value.length; j++) {
+          products[j - 1] = products[j]
+        }
+      }
+    }
+  }
+
+  export default {
+    return { count, doubleCount, products, addProduct, removeProduct }
+  }
 })
